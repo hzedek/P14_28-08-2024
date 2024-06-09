6e9da69ce6c53a89718a61cb47ff7ad812a7e853
LouisMichelin <louis.michelin097@gmail.com>
2024-06-09 17:26:54 +0200

    Avancements : Added state.json + CSS Optimizations

--- hrnet_react/src/pages/Home/Home.jsx
+++ hrnet_react/src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 import "./Home.scss";
+import { states } from "../../data/states.json";
 
 function Home() {
+   const listStates = states;
+
    return (
       <>
          <main>
@@ -9,35 +12,49 @@ function Home() {
                <a href="/employees">View Current Employees</a>
                <h2>Create Employee</h2>
                <form action="#" id="create-employee">
-                  <label>First Name</label>
-                  <input type="text" id="first-name" />
+                  <label htmlFor="first-name">First Name</label>
+                  <input type="text" id="first-name" placeholder="Firstname" />
 
-                  <label>Last Name</label>
-                  <input type="text" id="last-name" />
+                  <label htmlFor="last-name">Last Name</label>
+                  <input type="text" id="last-name" placeholder="Lastname" />
 
-                  <label>Date of Birth</label>
-                  <input id="date-of-birth" type="text" />
+                  <label htmlFor="date-of-birth">Date of Birth</label>
+                  <input type="date" id="date-of-birth" min="1924-01-01" />
 
-                  <label>Start Date</label>
-                  <input id="start-date" type="text" />
+                  <label htmlFor="start-date">Start Date</label>
+                  <input type="date" id="start-date" min="1950-01-01" />
 
-                  <fieldset>
+                  <fieldset className="FieldsetAddress">
                      <legend>Address</legend>
 
                      <label>Street</label>
-                     <input id="street" type="text" />
+                     <input id="street" type="text" placeholder="Street Name" />
 
                      <label>City</label>
-                     <input id="city" type="text" />
+                     <input id="city" type="text" placeholder="City Name" />
 
                      <label>State</label>
-                     <select id="state"></select>
+                     <select id="state">
+                        {listStates.map((state, index) => {
+                           return (
+                              <option value={index} key={index}>
+                                 {state.name}
+                              </option>
+                           );
+                        })}
+                     </select>
 
                      <label>Zip Code</label>
-                     <input id="zip-code" type="number" />
+                     <input
+                        id="zip-code"
+                        type="number"
+                        placeholder="Zip Code Number"
+                        min="1"
+                        max="99999"
+                     />
                   </fieldset>
 
-                  <label>Department</label>
+                  <label htmlFor="department">Department</label>
                   <select id="department">
                      <option>Sales</option>
                      <option>Marketing</option>
@@ -47,7 +64,7 @@ function Home() {
                   </select>
                </form>
 
-               <button>Save</button>
+               <button id="save-button">Save</button>
             </section>
             <div id="confirmation">Employee Created!</div>
          </main>
