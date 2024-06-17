10f92a081395eec1d3e83a16ab35fc7f611bbe59
LouisMichelin <louis.michelin097@gmail.com>
2024-06-17 21:54:53 +0200

    Setup des Datas Récupérées dans le <FORM/>

--- hrnet_react/src/pages/Home/Home.jsx
+++ hrnet_react/src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import "./Home.scss";
+import { useState } from "react";
 import { states } from "../../data/states.json";
 import Calendar from "../../components/Calendar/Calendar";
 
 function Home() {
+   // Liste des Etats
    const listStates = states;
+
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const [birthDay, setBirthDay] = useState(null);
+   const [startDay, setStartDay] = useState(null);
+   const [addressStreet, setAddressStreet] = useState("");
+   const [addressCity, setAddressCity] = useState("");
+   const [addressState, setAddressState] = useState("");
+   const [addressZipcode, setAddressZipcode] = useState("");
+   const [department, setDepartment] = useState("");
 
    return (
       <>
@@ -12,30 +24,79 @@ function Home() {
             <section className="CreateEmployees">
                <a href="/employees">View Current Employees</a>
                <h2>Create Employee</h2>
-               <form action="#" id="create-employee">
+               <form
+                  onSubmit={() =>
+                     console.log(
+                        firstName,
+                        lastName,
+                        birthDay,
+                        startDay,
+                        addressStreet,
+                        addressCity,
+                        addressState,
+                        addressZipcode,
+                        department
+                     )
+                  }
+                  id="create-employee"
+               >
                   <label htmlFor="first-name">First Name</label>
-                  <input type="text" id="first-name" placeholder="Firstname" />
+                  <input
+                     type="text"
+                     id="first-name"
+                     placeholder="Firstname"
+                     value={firstName}
+                     onChange={(e) => setFirstName(e.target.value)}
+                  />
 
                   <label htmlFor="last-name">Last Name</label>
-                  <input type="text" id="last-name" placeholder="Lastname" />
+                  <input
+                     type="text"
+                     id="last-name"
+                     placeholder="Lastname"
+                     value={lastName}
+                     onChange={(e) => setLastName(e.target.value)}
+                  />
 
                   <label htmlFor="date-of-birth">Date of Birth</label>
-                  <Calendar />
+                  <Calendar
+                     value={birthDay}
+                     onChange={(e) => setBirthDay(e.target.value)}
+                  />
 
                   <label htmlFor="start-date">Start Date</label>
-                  <Calendar />
+                  <Calendar
+                     value={startDay}
+                     onChange={(e) => setStartDay(e.target.value)}
+                  />
 
                   <fieldset className="FieldsetAddress">
                      <legend>Address</legend>
 
                      <label>Street</label>
-                     <input id="street" type="text" placeholder="Street Name" />
+                     <input
+                        id="street"
+                        type="text"
+                        placeholder="Street Name"
+                        value={addressStreet}
+                        onChange={(e) => setAddressStreet(e.target.value)}
+                     />
 
                      <label>City</label>
-                     <input id="city" type="text" placeholder="City Name" />
+                     <input
+                        id="city"
+                        type="text"
+                        placeholder="City Name"
+                        value={addressCity}
+                        onChange={(e) => setAddressCity(e.target.value)}
+                     />
 
                      <label>State</label>
-                     <select id="state">
+                     <select
+                        id="state"
+                        value={addressState}
+                        onChange={(e) => setAddressState(e.target.value)}
+                     >
                         {listStates.map((state, index) => {
                            return (
                               <option value={index} key={index}>
@@ -52,20 +113,25 @@ function Home() {
                         placeholder="Zip Code Number"
                         min="1"
                         max="99999"
+                        value={addressZipcode}
+                        onChange={(e) => setAddressZipcode(e.target.value)}
                      />
                   </fieldset>
 
                   <label htmlFor="department">Department</label>
-                  <select id="department">
+                  <select
+                     id="department"
+                     value={department}
+                     onChange={(e) => setDepartment(e.target.value)}
+                  >
                      <option>Sales</option>
                      <option>Marketing</option>
                      <option>Engineering</option>
                      <option>Human Resources</option>
                      <option>Legal</option>
                   </select>
+                  <button id="save-button">Save</button>
                </form>
-
-               <button id="save-button">Save</button>
             </section>
             <div id="confirmation">Employee Created!</div>
          </main>
