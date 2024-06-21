bd19fb0ef9e81fef4bc2f31ab3446aad13261edb
LouisMichelin <louis.michelin097@gmail.com>
2024-06-21 16:36:38 +0200

    Setup GridJS : OK

--- hrnet_react/src/pages/Employees/Employees.jsx
+++ hrnet_react/src/pages/Employees/Employees.jsx
@@ -1,13 +1,37 @@
 import "./Employees.scss";
+import { useState } from "react";
+import { Grid } from "gridjs-react";
+import "gridjs/dist/theme/mermaid.css";
 
 function Employees() {
+   const row = () => ["louis", "louis@mail.fr"];
+   const [data, setData] = useState([row()]);
+   const update = () => {
+      setData(data.slice(0).concat([row()]));
+   };
+
    return (
       <>
          <main>
             <div id="employee-div">
-               <h1>Current Employees</h1>
-               <table id="employee-table"></table>
                <a href="/">Home</a>
+               <h1>Current Employees</h1>
+               <button
+                  onClick={update.bind(this)}
+                  className={
+                     "py-2 mb-4 px-4 border rounded-md text-white bg-blue-600"
+                  }
+               >
+                  Add record
+               </button>
+
+               <Grid
+                  data={data}
+                  columns={["Name", "Email"]}
+                  pagination={{
+                     limit: 10,
+                  }}
+               />
             </div>
          </main>
       </>
