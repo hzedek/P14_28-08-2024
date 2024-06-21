f836484ad21374f4e72821a41d82a649fcbd24ee
LouisMichelin <louis.michelin097@gmail.com>
2024-06-21 18:25:12 +0200

    Setup du Grid + Manipulation des Datas depuis LocalStorage : en voie de fonctionner

--- hrnet_react/src/pages/Employees/Employees.jsx
+++ hrnet_react/src/pages/Employees/Employees.jsx
@@ -1,15 +1,40 @@
 import "./Employees.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Grid } from "gridjs-react";
 import "gridjs/dist/theme/mermaid.css";
 
 function Employees() {
    const row = () => ["louis", "louis@mail.fr"];
    const [data, setData] = useState([row()]);
-
    const update = () => {
       setData(data.slice(0).concat([row()]));
    };
+
+   const [importedLocal, setImportedLocal] = useState([]);
+
+   useEffect(() => {
+      const storage = { ...localStorage };
+      // console.log(localStorage.key(index));
+      // console.log({ ...localStorage });
+      // console.log(storage);
+      // setImportedLocal(storage);
+      // console.log("imported test", importedLocal);
+      setImportedLocal([...importedLocal, storage]);
+      console.log(importedLocal);
+   }, []);
+
+   for (let i = 0; i < localStorage.length; i++) {
+      let result;
+      result = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      // console.log(localStorage.getItem(localStorage.key(i)));
+      if (result.firstName === "Louis") {
+         console.log("YESS LOUIS");
+      } else {
+         console.log("eh bah non pas Louis");
+      }
+      // console.log(JSON.parse(result));
+      console.log(result);
+   }
 
    return (
       <>
