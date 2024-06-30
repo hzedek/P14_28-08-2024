de30a6cb956bb384229717f71c457e55e183894f
LouisMichelin <louis.michelin097@gmail.com>
2024-06-30 18:13:19 +0200

    HUGE BACKUP 2 : Import Problem from NPM almost solved (logical error)

---
+++ hrnet_react_rework/src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+// import "./Modal.scss";
+
+// export function openModal() {
+//    document.getElementById("validationModal").style.display = "block";
+// }
+// export function closeModal() {
+//    document.getElementById("validationModal").style.display = "none";
+// }
+
+// // EDIT DEPUIS JS LE CSS
+
+// function Modal() {
+//    return (
+//       <>
+//          <div id="validationModal">
+//             <button id="closeValidationModal" onClick={closeModal}></button>
+//             <div id="confirmation">Employee Created !</div>
+//          </div>
+//       </>
+//    );
+// }
+// export default Modal;
---
+++ hrnet_react_rework/src/pages/Employees/Employees.jsx
@@ -0,0 +1,50 @@
+import "./Employees.scss";
+import { Grid } from "gridjs-react";
+import "gridjs/dist/theme/mermaid.css";
+import HumanRessources from "../../../src/data/formdata.json";
+
+function Employees() {
+   return (
+      <>
+         <header id="WHealth-Header">
+            <div id="WHealth-Logo-Container_Employees">
+               <img
+                  id="WHealth-Logo"
+                  src="./src/assets/WHealthLogoEdited.png"
+                  alt="Logo WealthHealth"
+                  onClick={() => (location.href = "/")}
+               />
+            </div>
+            <a id="WHealth-LinkBtn" href="/">
+               Home
+            </a>
+         </header>
+         <main>
+            <section className="WHealth-GridSection">
+               <h1 className="WHealth-Title">Employees</h1>
+               <span className="employees-grid">
+                  <Grid
+                     data={HumanRessources}
+                     columns={[
+                        "First Name",
+                        "Last Name",
+                        "Start Day",
+                        "Department",
+                        "Birth Day",
+                        "Street",
+                        "City",
+                        "State",
+                        "Zip Code",
+                     ]}
+                     pagination={{
+                        limit: 7,
+                     }}
+                  />
+               </span>
+            </section>
+         </main>
+      </>
+   );
+}
+
+export default Employees;
