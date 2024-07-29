8ecd32a8ff59a7e19654503219b9a21cde76b1e0
LouisMichelin <louis.michelin097@gmail.com>
2024-07-29 17:56:35 +0200

    Backup : Redux folder + setups + slices (early)

--- hrnet_react_rework/src/index.jsx
+++ hrnet_react_rework/src/index.jsx
@@ -6,20 +6,22 @@ import Employees from "./pages/Employees/Employees";
 import "./index.scss";
 // REDUX Update
 // import { configureStore } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-
-const store = configureStore();
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
       <Provider store={store}>
-         <Router>
-            <Routes>
-               <Route path="/" index element={<Home />} />
-               <Route path="/employees" index element={<Employees />} />
-            </Routes>
-         </Router>
+         <PersistGate persistor={persistor}>
+            <Router>
+               <Routes>
+                  <Route path="/" index element={<Home />} />
+                  <Route path="/employees" index element={<Employees />} />
+               </Routes>
+            </Router>
+         </PersistGate>
       </Provider>
    </React.StrictMode>
 );
---
+++ hrnet_react_rework/src/redux/store.js
@@ -0,0 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import formulaireSlice from "./slices/formulaire/formulaireSlice";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+//////////////////////////////////
+// SETUP ROOT-REDUCER & PERSIST //
+//////////////////////////////////
+const persistConfig = {
+   key: "root",
+   storage,
+   version: 1,
+};
+const rootReducer = combineReducers({
+   formulaire: formulaireSlice,
+});
+
+///////////////////////////////////////
+// SETUP STORE & PERSISTOR + EXPORTS //
+///////////////////////////////////////
+
+// PERSIST REDUCER
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// EXPORTS
+export const store = configureStore({
+   reducer: persistedReducer,
+   // REMOVE DEFAULT ERROR CHECKER
+   // middleware: (getDefaultMiddleware) =>
+   //    getDefaultMiddleware({
+   //       serializableCheck: false,
+   //    }),
+});
+export const persistor = persistStore(store);
