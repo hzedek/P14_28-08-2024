819be8107d73752212a9acf86fec6ba99fd1aad9
LouisMichelin <louis.michelin097@gmail.com>
2024-07-29 16:38:15 +0200

    First redux-version push

--- hrnet_react_rework/src/index.jsx
+++ hrnet_react_rework/src/index.jsx
@@ -4,14 +4,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Employees from "./pages/Employees/Employees";
 import "./index.scss";
+// REDUX Update
+// import { configureStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
-      <Router>
-         <Routes>
-            <Route path="/" index element={<Home />} />
-            <Route path="/employees" index element={<Employees />} />
-         </Routes>
-      </Router>
+      <Provider store={store}>
+         <Router>
+            <Routes>
+               <Route path="/" index element={<Home />} />
+               <Route path="/employees" index element={<Employees />} />
+            </Routes>
+         </Router>
+      </Provider>
    </React.StrictMode>
 );
